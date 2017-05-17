dc58847665c28652575c576efad62b882d0fd443
david <root@localhost.localdomain>
2017-05-17 14:39:43 -0400

    Fixed some typoes and package references

--- davids-build-script.sh
+++ davids-build-script.sh
@@ -2,7 +2,12 @@
 
 printf "Setup our build environment\n"
 apt-get install live-build -y
+if [ -d "live-build-config" ]; then
+	printf "you appear to already have your build environment downloaded...\n"
+else
 git clone git://git.kali.org/live-build-config.git
+fi
+
 cd live-build-config
 
 printf "adding packages not included in kali\n"
@@ -26,8 +31,8 @@ echo "open-vm-tools-dkms" >> kali-config/common/package-lists/kali.list.chroot
 
 printf "copying .deb files to packages folder (automatically installed)\n"
 mkdir -p kali-config/common/packages.chroot
-cp packages/nessus_6.10.5_amd64.deb kali-config/common/packages.chroot/
-cp packages/nomachine_5.2.21_1_amd64.deb kali-config/common/packages.chroot/
+cp ../packages/nessus*.deb kali-config/common/packages.chroot/
+cp ../packages/nomachine*.deb kali-config/common/packages.chroot/
 
 # CHROOT OVERLAY
 # kali-config/common/includes.chroot/
@@ -40,38 +45,38 @@ else
 	printf "WARNING no public key detected in /root/.ssh/authorized_keys\n"
 fi
 
-if [ -f packages/VMware-Player-12.5.5-5234757.x86_64.bundle ]; then
+if [ -f ../packages/VMware*.bundle ]; then
 	printf "Adding VMware player to /root (manual install required)\n"
-	cp packages/VMware-Player-12.5.5-5234757.x86_64.bundle kali-config/common/includes.chroot/root/
+	cp ../packages/VMware*.bundle kali-config/common/includes.chroot/root/
 else
 	printf "WARNING no VMware Player binary in packages folder\n"
 fi
 
-if [ -f packages/burpsuite_pro.jar ]; then
+if [ -f ../packages/burpsuite*.jar ]; then
 	printf "Adding Burp to /root (manual install required)\n"
-	cp packages/burpsuite_pro.jar kali-config/common/includes.chroot/root/
+	cp ../packages/burpsuite*.jar kali-config/common/includes.chroot/root/
 else
 	printf "WARNING no burp.jar in packages folder\n"
 fi
 
-if [ -f packages/IE11.Win7.For.Windows.VMware/IE11_-_Win7-disk1.vmdk ]; then
+if [ -f ../packages/IE11.Win7.For.Windows.VMware/IE11_-_Win7-disk1.vmdk ]; then
 	printf "Adding Windows VM image in /root\n"
-	cp -r packages/IE11.Win7.For.Windows.VMware kali-config/common/includes.chroot/root/
+	cp -r ../packages/IE11.Win7.For.Windows.VMware kali-config/common/includes.chroot/root/
 else
 	printf "WARNING no windows VM found in packages folder\n"
 fi
 
 #configs
 printf "Modifying default vim, screen, and python configs\n"
-cp packages/vimrc kali-config/common/includes.chroot/root/.vimrc
-cp packages/pythonrc kali-config/common/includes.chroot/root/.pythonrc
-cp packages/screenrc kali-config/common/includes.chroot/root/.screenrc
+cp ../packages/vimrc kali-config/common/includes.chroot/root/.vimrc
+cp ../packages/pythonrc kali-config/common/includes.chroot/root/.pythonrc
+cp ../packages/screenrc kali-config/common/includes.chroot/root/.screenrc
 
 # CHROOT BOOT HOOKS
 # kali-config/common/hooks/
 # these scripts run every time the server boots
 
-printf("Adding chroot boot hooks\n")
+printf "Adding chroot boot hooks\n"
 mkdir -p kali-config/common/hooks/
 
 # enable SSH server
@@ -99,9 +104,9 @@ chmod +x kali-config/common/hooks/05-update-nikto.chroot
 # preseed file for unattended install 
 printf "Fetching preseed file for unattended install\n"
 mkdir -p kali-config/common/includes.installer
-wget https://raw.githubusercontent.com/kafkaesqu3/custom_kali/master/kali-config/common/includes.installer/preseed.cfg -O ./kali-config/common/includes.installer/preseed.cfg 
+cp preseed.cfg kali-config/common/includes.installer/preseed.cfg 
 
-printf "adding boot menu options that will process preseed file"
+printf "adding boot menu options that will process preseed file\n"
 # ref http://www.syslinux.org/wiki/index.php?title=Config
 cat << EOF > kali-config/common/includes.binary/isolinux/install.cfg
 label install
@@ -126,5 +131,5 @@ if [[ $input == 'y' ]]; then
 else
 	printf "Not building at this time\n"
 	printf "Execute the following command when you're ready to build: \n"
-	printf "./build.sh --variant gnome --distribution kali-rolling --verbose"
+	printf "./build.sh --variant gnome --distribution kali-rolling --verbose\n"
 fi
